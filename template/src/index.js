dda7ef816a2295aa1abb60c0876435a28706e622
SO-JIMM <jamesdgrace@gmail.com>
2020-02-25 22:24:11 +1100

    fixed base and settings ready for npm

--- template/src/index.js
+++ template/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-// import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
